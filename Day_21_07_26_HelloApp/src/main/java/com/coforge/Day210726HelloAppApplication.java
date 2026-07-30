e66f30d1b40d7e844b0411e905e1743fc79c7381
Vaishnavi.Kutala <Vaishnavi.Kutala@LP-PUN-0X138951.IN.COFORGETECH.COM>
2026-07-30 12:05:23 +0530

    Added projects

---
+++ Day_21_07_26_HelloApp/src/main/java/com/coforge/Day210726HelloAppApplication.java
@@ -0,0 +1,13 @@
+package com.coforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day210726HelloAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day210726HelloAppApplication.class, args);
+	}
+
+}
